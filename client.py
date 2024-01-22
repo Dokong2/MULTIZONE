26ee4d1007fe5c8a1a42076a4b997d419932d47b
Dukong <jiwan6942@gmail.com>
2024-01-22 19:26:29 +0900

    버그 천국 ㅋㅋㅋㅋㅋㅋ

--- client.py
+++ client.py
@@ -4,8 +4,18 @@ host = "127.0.0.1"
 port = 0
 
 
-global host
-global port
+def recv():
+    while True:
+        data = client_socket.recv(1024).decode()
+        print(data)
+
+def chat():
+    chattext = input("PORT : " + str(port) + " ㅣ>>")
+    client_socket.send(chattext)
+
+# start
+
+
 print("MULTIZONE ㅣ client")
 print("호완 버전 : 1.0")
 print("서버에 접속하려면 [connect]")
@@ -14,29 +24,13 @@ while True:
     if start == "connect":
         print("포트를 입력하세요.")
         port = input(">>")
-        connect()
     else:
         print("입력값이 잘못 되었습니다.")
-    try:
-        global host
-        global port
-        client_socket = socket.socket()
-        client_socket.connect((host, int(port)))
-        print("서버에 연결을 성공했습니다.")
-    except TimeoutError:
-        print("에러 : 서버가 응답하지 않습니다. 서버가 켜져있는지 확인하고 인터넷에 연결되었는지 확인하세요.")
-    except OverflowError:
-        print("에러 : 포트 값이 너무 큽니다. 포트의 범위는 [0~65535] 안입니다.")
-    except ValueError:
-        print("에러 : 포트값이 유효하지 않습니다. [에러 : 포트번호는 숫자입니다.]")
-    except OSError:
-        print("에러 : 아이피 주소가 올바르지 않습니다. 아이피 주소가 유효한지 확인하세요.")
-    except:
-        print("에러 : 알 수 없는 에러가 발생했습니다. 도움이 된다면 스크린샷을 보내주시면 감사하겠습니다. 추후 패치하겠습니다.")
 
-
-def recv():
-    while True:
-        data = conn.recv(1024).decode()
-
-        #와 검나 복잡햌ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+    client_socket = socket.socket()
+    client_socket.connect((host, int(port)))
+    print("서버에 연결을 성공했습니다.")
+    thread1 = threading.Thread(target=recv)
+    thread1.start()
+    thread2 = threading.Thread(target=chat)
+    thread2.start()
--- server.py
+++ server.py
@@ -7,55 +7,47 @@ import time
 server_socket = None
 connlist = []
 now = time
-#?
-while True:
-    os.system('cls')
-    print("MULTIZONE ㅣ Server")
-    print("호완 버전 : 1.0")
-    print("서버를 시작하려면 [serverstart]")
-    start = input(">>")
-    if start == "serverstart":
-        try:
-            print("포트를 입력하세요.")
-            port = int(input(">>"))
-            host = "127.0.0.1"
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print("서버 소켓이 생성되었습니다!")
-            server_socket.bind((host, port))
-            print("서버 소켓이 바인드 되었습니다.")
-            print("서버 생성이 완료되었습니다.")
-        except ValueError:
-            print("포트 값이 유효하지 않습니다. 오타를 확인해주세요.")
-            print("홈으로 돌아가려면 아무 값이나 입력하세요.")
-            input("")
-        except OSError:
-            print("아이피 주소에 이상이 있습니다. 아이피 주소가 정상정으로 설정됬는지 확인해주세요.")
-            print("홈으로 돌아가려면 아무 값이나 입력하세요.")
-            input("")
-        except OverflowError:
-            print("에러 : 포트 값이 너무 큽니다. 포트값의 범위는 [0~65535]입니다.")
-            print("홈으로 돌아가려면 아무 값이나 입력하세요.")
-            input("")
-        except:
-            print("알수 없는 에러가 났습니다.")
-            print("홈으로 돌아가려면 아무 값이나 입력하세요.")
-            input("")
-
-
+conn = ""
 def accepts():
     global server_socket
     server_socket.listen(2)
     while True:
         conn, address = server_socket.accept()
-        print(now.strftime('%Y-%m-%d %H:%M:%S'), "클라이언트가 접속했습니다." + "주소 : " + str(address))
+        print(now.strftime('%Y-%m-%d %H:%M:%S'), "client is connect. " + "way : " + str(address))
         connlist.append(conn)
+        print("[connlist]에 새로운 콘이 들어갔습니다.")
+        t1 = threading.Thread(target=recvingandsend,args=conn)
+        t1.start()
 
-def recvingandsend():
+
+def recvingandsend(conn):
     global connlist
-    global conn
-    while True:
-        data = conn.recv(1024).decode()
-        print(data)
-        for connlist in conn:
-            conn.send(data.encode())
+    data = conn.recv(1024).decode()
+    print("클라이언트에게 샌드 데이터가 왔습니다. 데이터 : " + str(data))
+    for connlist in conn:
+        conn.send(data.encode())
+        print("모든 클라이언트에게 전송되었습니다. 값 : " + str(sendmsg))
 
+
+
+
+
+# start
+
+
+os.system('cls')
+print("MULTIZONE ㅣ Server")
+print("호완 버전 : 1.0")
+print("서버를 시작하려면 [open]")
+start = input(">>")
+if start == "open":
+    print("포트")
+    port = int(input(">>"))
+    host = "127.0.0.1"
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("소켓 서버 생성")
+    server_socket.bind((host, port))
+    print("소켓 서버 바인드 성공")
+    print("서버가 생성되었습니다.")
+    t2 = threading.Thread(target=accepts)
+    t2.start()
